658bcdab49771a3a9cbcc96e1fc4200b494ebe63
Altamiro Bruno <altamiro_bruno@yahoo.com.br>
2025-06-28 13:05:30 -0300

    feat: initialize Nuxt 3 application with essential configurations and modules
    - Added ESLint configuration for code quality.
    - Created nuxt.config.ts with compatibility settings and essential modules.
    - Set up package.json with dependencies for Nuxt, TypeScript, and related modules.
    - Developed index.vue page for user input with WhatsApp scheduling.
    - Included favicon.ico for branding.
    - Added robots.txt for search engine optimization.
    - Configured TypeScript settings for server and project.

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="min-h-dvh flex items-center flex-col justify-center">
+    <div class="max-w-[960px] mx-auto w-full">
+      <h1 class="text-2xl lg:text-[2rem] font-semibold text-center">
+        Digite o whatsapp da Barberia<br >
+        que deseja fazer seu agendamento
+      </h1>
+      <div class="flex flex-col items-center mt-8">
+        <UInput
+          v-model="models.whatsapp"
+          size="lg"
+          class="w-80"
+          placeholder="(99) 99999-9999"
+        />
+        <UButton label="Ir para agendamento" class="mt-6" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface models {
+  whatsapp: string;
+}
+
+const models = ref<models>({
+  whatsapp: "",
+});
+</script>
